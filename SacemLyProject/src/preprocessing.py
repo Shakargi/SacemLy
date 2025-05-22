d0eaad97a33b3f774ef629915ee48814bb0c7026
Shakargi <avrahamsh124@gmail.com>
2025-05-22 20:13:22 +0300

    Update preprocessing.py

---
+++ SacemLyProject/src/preprocessing.py
@@ -0,0 +1,25 @@
+import re
+import nltk
+import numpy as np
+from nltk.corpus import stopwords
+
+nltk.download('wordnet')
+nltk.download('stopwords')
+stopWords = stopwords.words('english')
+
+def clean_and_tockenize(text):
+
+    cleanedText = text.lower()
+    cleanedText = re.sub(r'[^\w\s]', '', cleanedText).split()
+
+    lemmatizer = nltk.stem.WordNetLemmatizer()
+    cleanedFinal = []
+    for word in cleanedText:
+        if not word in stopWords:
+            wordAdd = lemmatizer.lemmatize(word)
+            cleanedFinal.append(wordAdd)
+
+    return np.array(cleanedFinal)
+
+
+
