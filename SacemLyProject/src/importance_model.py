bcea25f4ad41a0977d7c2273c37613949bf1a070
Shakargi <avrahamsh124@gmail.com>
2025-05-24 16:37:55 +0300

    Update importance_model.py

---
+++ SacemLyProject/src/importance_model.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+words = ["I", "like", "to", "eat", "pizza", "with", "cheese", "and", "tomato"]
+
+def buildVocab(words):
+    uniqueWords = sorted(set(words))
+    return {Word : i for i, Word in enumerate(uniqueWords)}
+
+def buildReverseVocab(words):
+    uniqueWords = sorted(set(words))
+    return {i: Word for i, Word in enumerate(uniqueWords)}
+
+
+vocab = buildVocab(words)
+reverseVocab = buildReverseVocab(words)
+
+def generatePairs(words, windowSize = 2):
+    data = []
+    for i in range(windowSize, len(words) - windowSize):
+        context = []
+        for j in range(-windowSize, windowSize+1):
+            if j!=0:
+                context.append(words[i+j])
+        data.append((context, words[i]))
+
+
+    return data
+
+
+def oneHotEncoder(vocab, word):
+    index = vocab[word]
+    return np.eye(len(vocab))[index]
+
+def onHotDecoder(reverseVocab, vector):
+    return reverseVocab[np.argmax(vector)]
+
+
+def prepareData(vocab, pairs):
+    data = []
+    for i in range(len(pairs)):
+        onehot = np.zeros(len(vocab))
+        context = pairs[i][0]
+        target = pairs[i][1]
+        for j in range(len(context)):
+            onehot += oneHotEncoder(vocab, context[j])
+        onehot /= len(context)
+        data.append((onehot, oneHotEncoder(vocab, target)))
+
+    return data
+
