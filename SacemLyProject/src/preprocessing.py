a5fb830259ab932d890ce15c3b6958d8cd5679e9
Shakargi <avrahamsh124@gmail.com>
2025-05-24 22:55:02 +0300

    Update preprocessing.py

--- SacemLyProject/src/preprocessing.py
+++ SacemLyProject/src/preprocessing.py
@@ -12,14 +12,14 @@ def clean_and_tockenize(text):
     cleanedText = text.lower()
     cleanedText = re.sub(r'[^\w\s]', '', cleanedText).split()
 
-    lemmatizer = nltk.stem.WordNetLemmatizer()
-    cleanedFinal = []
-    for word in cleanedText:
-        if not word in stopWords:
-            wordAdd = lemmatizer.lemmatize(word)
-            cleanedFinal.append(wordAdd)
+    # lemmatizer = nltk.stem.WordNetLemmatizer()
+    # cleanedFinal = []
+    # for word in cleanedText:
+    #     if not word in stopWords:
+    #         wordAdd = lemmatizer.lemmatize(word)
+    #         cleanedFinal.append(wordAdd)
+    #
+    # return np.array(cleanedFinal)
 
-    return np.array(cleanedFinal)
-
-
+    return np.array(cleanedText)
 
