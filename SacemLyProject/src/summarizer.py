f39f23e63c29b504440d0e5eef966f43e86e3b24
Shakargi <avrahamsh124@gmail.com>
2025-06-25 00:52:33 +0300

    Update summarizer.py

--- SacemLyProject/src/summarizer.py
+++ SacemLyProject/src/summarizer.py
@@ -3,25 +3,31 @@ import LSA_keywords as lsa
 import importance_model as im
 import preprocessing as pre
 
-def summerizer(text, title, embeddingMatrix, precentSummerization=0.3):
+
+def summarizer(text, title, embeddingMatrix, numSentences=20):
     words = pre.clean_and_tockenize(text)
     vocab = im.buildVocab(words)
     sentences = im.sentencesSplitter(text)
     keyWords = lsa.keywords(sentences)
+    print(keyWords)
     features = im.extract_features(text, embeddingMatrix, vocab, keyWords, title)
-    ranking = [featureOfsentence["importanceScore"] for featureOfsentence in features]
-    numSentences = int(np.round(len(ranking) * precentSummerization))
 
-    print(f"Ranking: {np.array(ranking)}")
+    keyWordDensity = [featureOfsentence["keywordDensity"] for featureOfsentence in features]
+    similarityToTitle = [featureOfsentence["similarityToTitle"] for featureOfsentence in features]
+    importanceScore = [featureOfsentence["importanceScore"] for featureOfsentence in features]
 
-    indexSentences = np.argsort(ranking)[::-1]
-    indexSentences = indexSentences[:numSentences+1]
+    score = 0.5 * np.array(importanceScore) + 0.3 * np.array(similarityToTitle) + 0.2 * np.array(keyWordDensity)
+
+    if numSentences > len(score):
+        numSentences = max(int(np.ceil(len(score) * 0.2)), 1)
+
+
+    indexSentences = np.argsort(score)[::-1]
+    indexSentences = indexSentences[:numSentences]
 
     indexSentences.sort()
     summerizedSentences = [sentences[i] for i in indexSentences]
 
-    for sentence in summerizedSentences:
-        print(sentence)
-
+    return summerizedSentences
 
 
